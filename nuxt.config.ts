c870d4c0363c87e3641586d2238181a47d7f343f
jvm2000 <minozajohnvincent2000@gmail.com>
2025-09-10 07:22:59 +0800

    fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
   runtimeConfig: {
     public: {
-      apiBase: 'laravel.restaurolegalservices.com', // Laravel API
+      apiBase: '',
     },
     head: {
       meta: [{ name: 'csrf-token', content: '' }],
